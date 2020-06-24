76dbf207e45eaf9af36c70f0c71c622d4cb2ba1e
TulioCaz <tuliocaz.windcoast@gmail.com>
2020-06-24 14:50:47 -0300

    fix: removing bugs

--- src/controller/Services/apiHttpRequestService.js
+++ src/controller/Services/apiHttpRequestService.js
@@ -8,12 +8,12 @@ module.exports = {
 
     const api_key = 'cL7kvfhq0HL3HEoVeFTIEJyXfcwJ10kF';
     const q = titleForSearch;
-    const limit = '1';
-    const offset = '0';
-    const rating = 'G';
+    const limit = 1;
+    const offset = 0;
+    const rating = 'g';
     const lang = 'en';
 
-    const response = await api.get('/api', {
+    const response = await api.get('/search', {
       params: { api_key, q, limit, offset, rating, lang },
     });
     return response.data;
