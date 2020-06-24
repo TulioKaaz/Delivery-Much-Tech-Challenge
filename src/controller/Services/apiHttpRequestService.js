3a51f83849630641bd10dd355819582e82ac927a
TulioCaz <tuliocaz.windcoast@gmail.com>
2020-06-24 13:39:41 -0300

    feat: service to search for data in the APIs, created

---
+++ src/controller/Services/apiHttpRequestService.js
@@ -0,0 +1,32 @@
+const axios = require('axios');
+
+module.exports = {
+  async requestGiphyApi(titleForSearch) {
+    const api = axios.create({
+      baseURL: 'https://api.giphy.com/v1/gifs',
+    });
+
+    const api_key = 'cL7kvfhq0HL3HEoVeFTIEJyXfcwJ10kF';
+    const q = titleForSearch;
+    const limit = '1';
+    const offset = '0';
+    const rating = 'G';
+    const lang = 'en';
+
+    const response = await api.get('/api', {
+      params: { api_key, q, limit, offset, rating, lang },
+    });
+    return response.data;
+  },
+
+  async requestRecipepuppyApi(ingredients) {
+    const api = axios.create({
+      baseURL: 'http://www.recipepuppy.com',
+    });
+
+    const i = ingredients.join(',');
+
+    const response = await api.get('/api', { params: { i } });
+    return response.data;
+  },
+};
